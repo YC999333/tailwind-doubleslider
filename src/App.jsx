2dbf468cc1be6b134fbb054ed7803f9cadb391e7
yc999333 <yc99993333@gmail.com>
2021-07-05 11:01:20 +0800

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DoubleSlider from "./DoubleSlider";
+
+function App() {
+  return (
+    <div className="text-center">
+      <DoubleSlider />
+    </div>
+  );
+}
+
+export default App;
